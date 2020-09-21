efe1a6cd38e672e33402bcc93de9afb07df93767
kirill <031kirill2a@mail.ru>
2020-09-21 16:18:38 +0300

    add some classes

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -19,10 +19,10 @@ public class Main {
         return "no";
     }
 
-    public static class Point {
-        public double x, y;
+    private static class Point {
+        private double x, y;
 
-        public Point (double x, double y) {
+        private Point (double x, double y) {
             this.x = x;
             this.y = y;
         }
@@ -41,20 +41,6 @@ public class Main {
     }
 
     public static void main (String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.print("enter the x and y for first point: ");
-        int x1 = scanner.nextInt();
-        int y1 = scanner.nextInt();
-        System.out.print("enter the x and y for second point: ");
-        int x2 = scanner.nextInt();
-        int y2 = scanner.nextInt();
-        System.out.print("enter the x and y for third point: ");
-        int x3 = scanner.nextInt();
-        int y3 = scanner.nextInt();
-        System.out.print("enter the x and y for fourth point: ");
-        int x4 = scanner.nextInt();
-        int y4 = scanner.nextInt();
-        System.out.printf("%s", isParallelogram(x1, x2, x3, x4, y1, y2, y3, y4));*/
         Point p1 = readPoint("p1");
         Point p2 = readPoint("p2");
         Point p3 = readPoint("p3");
