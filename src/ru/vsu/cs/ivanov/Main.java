b84fc67800c455f347c3c75e9bae1595bad0eb01
kirill <031kirill2a@mail.ru>
2020-09-23 13:42:33 +0300

    add some classes

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 import static java.lang.Math.abs;
 
-public class Main {
+public class Main
+{
 
-    public static void main (String[] args) {
+    public static void main (String[] args)
+    {
         Point p1 = readPoint("p1");
         Point p2 = readPoint("p2");
         Point p3 = readPoint("p3");
@@ -14,35 +16,43 @@ public class Main {
         System.out.printf(isParallelogram(p1, p2, p3, p4));
     }
 
-    private static String isParallelogram(Point p1,Point p2, Point p3, Point p4) {
-        if ((Math.abs(p1.x - p2.x) == Math.abs(p3.x - p4.x)) && (Math.abs(p1.y - p2.y) == Math.abs(p3.y - p4.y))) {
+    private static String isParallelogram(Point p1,Point p2, Point p3, Point p4)
+    {
+        if ((Math.abs(p1.x - p2.x) == Math.abs(p3.x - p4.x)) && (Math.abs(p1.y - p2.y) == Math.abs(p3.y - p4.y)))
+        {
             return "yes";
         }
-        if ((Math.abs(p1.x - p3.x) == Math.abs(p2.x - p4.x)) && (Math.abs(p1.y - p3.y) == Math.abs(p2.y - p4.y))) {
+        if ((Math.abs(p1.x - p3.x) == Math.abs(p2.x - p4.x)) && (Math.abs(p1.y - p3.y) == Math.abs(p2.y - p4.y)))
+        {
             return "yes";
         }
-        if ((Math.abs(p1.x - p4.x) == Math.abs(p3.x - p2.x)) && (Math.abs(p1.y - p4.y) == Math.abs(p3.y - p2.y))) {
+        if ((Math.abs(p1.x - p4.x) == Math.abs(p3.x - p2.x)) && (Math.abs(p1.y - p4.y) == Math.abs(p3.y - p2.y)))
+        {
             return "yes";
         }
         return "no";
     }
 
-    private static class Point {
+    private static class Point
+    {
         private double x, y;
 
-        private Point (double x, double y) {
+        private Point (double x, double y)
+        {
             this.x = x;
             this.y = y;
         }
     }
 
-    private static double readDouble (String name) {
+    private static double readDouble (String name)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("enter %s: ", name);
         return scanner.nextDouble();
     }
 
-    private static Point readPoint (String name) {
+    private static Point readPoint (String name)
+    {
         double x = readDouble (name + ".x");
         double y = readDouble (name + ".y");
         return new Point (x, y);
