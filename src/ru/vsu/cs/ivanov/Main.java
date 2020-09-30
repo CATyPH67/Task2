17b6f9b36265d6762759e7b91644677066f80d78
kirill <031kirill2a@mail.ru>
2020-09-30 15:47:58 +0300

    add some classes

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -6,11 +6,12 @@ import static java.lang.Math.abs;
 
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Point p1 = Program.readPoint("p1");
         Point p2 = Program.readPoint("p2");
         Point p3 = Program.readPoint("p3");
         Point p4 = Program.readPoint("p4");
-        System.out.printf(Program.isParallelogram(p1, p2, p3, p4));
+        System.out.print(Program.isParallelogram(p1, p2, p3, p4));
     }
 }
--- src/ru/vsu/cs/ivanov/Program.java
+++ src/ru/vsu/cs/ivanov/Program.java
@@ -2,36 +2,44 @@ package ru.vsu.cs.ivanov;
 
 import java.util.Scanner;
 
-public class Program {
-    public static Point readPoint(String name) {
-        double x = readDouble (name + ".x");
-        double y = readDouble (name + ".y");
-        return new Point (x, y);
+public class Program
+{
+    public static Point readPoint(String name)
+    {
+        double x = readDouble(name + ".x");
+        double y = readDouble(name + ".y");
+        return new Point(x, y);
     }
 
-    public static double readDouble(String name) {
+    public static double readDouble(String name)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("enter %s: ", name);
         return scanner.nextDouble();
     }
 
-    public static String isParallelogram(Point p1, Point p2, Point p3, Point p4) {
-        if (isParallel(p1, p2, p3, p4) && isParallel(p1, p3, p2, p4)) {
+    public static String isParallelogram(Point p1, Point p2, Point p3, Point p4)
+    {
+        if (isParallel(p1, p2, p3, p4) && isParallel(p1, p3, p2, p4))
+        {
             return "yes";
         }
 
-        if (isParallel(p1, p3, p2, p4) && isParallel(p1, p4, p2, p3)) {
+        if (isParallel(p1, p3, p2, p4) && isParallel(p1, p4, p2, p3))
+        {
             return "yes";
         }
 
-        if (isParallel(p1, p4, p2, p3) && isParallel(p1, p2, p4, p3)) {
+        if (isParallel(p1, p4, p2, p3) && isParallel(p1, p2, p4, p3))
+        {
             return "yes";
         }
 
         return "no";
     }
 
-    public static boolean isParallel(Point p1, Point p2, Point p3, Point p4) {
+    public static boolean isParallel(Point p1, Point p2, Point p3, Point p4)
+    {
         return ((p1.y - p2.y) / (p1.x - p2.x)) == ((p3.y - p4.y) / (p3.x - p4.x)) ||
                 ((p1.x == p2.x) && (p3.x == p4.x)) || ((p1.y == p2.y) && (p3.y == p4.y));
     }
