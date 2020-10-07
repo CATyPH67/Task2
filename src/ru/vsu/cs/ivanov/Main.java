f3fe1656cdfd0192310cb407b5d0b72bceaf26c9
kirill <031kirill2a@mail.ru>
2020-10-07 13:37:12 +0300

    add some classes

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -29,12 +29,12 @@ public class Main
 
     public static boolean isParallelogram(Point p1, Point p2, Point p3, Point p4)
     {
-        return (isParallel(p1, p2, p3, p4) && isParallel(p1, p3, p2, p4))
-            || (isParallel(p1, p3, p2, p4) && isParallel(p1, p4, p2, p3))
-            || (isParallel(p1, p4, p2, p3) && isParallel(p1, p2, p4, p3));
+        return (isLinesParallel(p1, p2, p3, p4) && isLinesParallel(p1, p3, p2, p4))
+            || (isLinesParallel(p1, p3, p2, p4) && isLinesParallel(p1, p4, p2, p3))
+            || (isLinesParallel(p1, p4, p2, p3) && isLinesParallel(p1, p2, p4, p3));
     }
 
-    public static boolean isParallel(Point p1, Point p2, Point p3, Point p4)
+    public static boolean isLinesParallel(Point p1, Point p2, Point p3, Point p4)
     {
         return ((p1.y - p2.y) / (p1.x - p2.x)) == ((p3.y - p4.y) / (p3.x - p4.x)) ||
                 ((p1.x == p2.x) && (p3.x == p4.x)) || ((p1.y == p2.y) && (p3.y == p4.y));
